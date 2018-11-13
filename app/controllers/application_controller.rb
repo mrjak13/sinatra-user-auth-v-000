ad27ea3cc91bbeaa102e5c07932f18cd98173fee
Justin Kenney <justinkenney13@gmail.com>
2018-11-13 19:28:10 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,9 +26,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
+    binding.pry
+
     @user = User.find_by(email: params["email"], password: params["password"])
-    session[:id] = @user.id
-    redirect '/users/home'
+    # if
+      session[:id] = @user.id
+      redirect '/users/home'
   end
 
   get '/sessions/logout' do
