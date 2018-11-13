929dba65942b01b24dfdac89ba9d76afc65e47ce
Justin Kenney <justinkenney13@gmail.com>
2018-11-13 19:33:31 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < Sinatra::Base
     erb :'sessions/login'
   end
 
-  post '/sessions' do  
+  post '/sessions' do
 
     @user = User.find_by(email: params["email"], password: params["password"])
     if @user != nil
