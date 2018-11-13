f8d5beb3de5ff728e551298a1c97f2e8c65f4077
Justin Kenney <justinkenney13@gmail.com>
2018-11-13 19:18:54 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-
+    @user = User.find_by(email: params["email"], password: params["password"])
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
@@ -36,7 +37,6 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-    
     @user = User.find(session[:id])
     erb :'/users/home'
   end
